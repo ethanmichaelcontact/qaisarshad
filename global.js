9d6e4e84378f031e5e08f52afc71cbeb64f3038d
ethanmichaelcontact <ethan@ethanmichael.contact>
2026-07-22 23:05:01 -0400

    Update global.js

--- global.js
+++ global.js
@@ -1,3 +1,6 @@
 document.getElementById("linkedIn_icon").addEventListener("click", () => {
     window.open("https://pk.linkedin.com/in/qais-arshad-922a322b1");
 });
+document.getElementById("contact").addEventListener("click", () => {
+    window.open("https://pk.linkedin.com/in/qais-arshad-922a322b1");
+});
